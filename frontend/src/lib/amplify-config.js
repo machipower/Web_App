73a79677ec45d255fc5cbd4a9ca8a0d5a4d9c071
machipower <machipower.service@gmail.com>
2025-05-31 13:27:24 +0800

    Update amplify-config.js

--- frontend/src/lib/amplify-config.js
+++ frontend/src/lib/amplify-config.js
@@ -1,8 +1,8 @@
 export const amplifyConfig = {
   Auth: {
-    region: "ap-southeast-2",
-    userPoolId: "ap-southeast-2_pUZWPptE2",
-    userPoolWebClientId: "2rufu41r5rgbdm19kp3ihjea41",
+    region: "",
+    userPoolId: "",
+    userPoolWebClientId: "",
   },
 };
 
