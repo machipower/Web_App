95b0bf2e32f2af697c92b658e3c457ee95092035
12Maggie11 <machipower.service@gmail.com>
2025-05-31 15:43:25 +0800

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // 加這一行指定輸出資料夾
+  },
 })
